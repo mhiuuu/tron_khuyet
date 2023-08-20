26870f40492fdadb084195b418cea84d4ab7145b
Mai Trung Hieu (Mathew) <maitrunghieu2287@gmail.com>
2023-08-21 00:19:42 +0700

    Started working on quotes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 /* Import the styling */
 import './Styling/index.css'
+/*Styling for the Hero section*/
 import './Styling/Hero_text.css'
 import './Styling/Hero.css'
 
 /* Import other components */
 import Hero from './Components/Hero';
 import Header from './Components/Navbar';
-
+import Quotes from './Components/Quotes';
 /* Import outsource library */
 
 
@@ -15,19 +16,22 @@ function App() {
   return(
     <div>
       {/* The navbar */}
-      <section>
+      <header>
         <Header />
-      </section> 
+      </header> 
 
-      {/* The first effect you will see */}
-      <section>
-        <Hero />
-      </section>
+      <main>
+        {/* The first effect you will see */}
+        <section>
+          <Hero />
+        </section>
 
-      {/* The gallery area */}
-      <section>
-
-      </section>
+        {/* The gallery area */}
+        <section>
+          <Quotes />
+        </section>
+      </main>
+      
     </div>
   );
 };
---
+++ src/Components/Quotes.jsx
@@ -0,0 +1,38 @@
+const Quotes = () => {
+  return (
+    <div className="container h-[60vh] mx-auto flex justify-center">
+      {/* First block */}
+      <section class="flex flex-col md:flex-row mx-10 lg:mx-20">
+        {/* The first quote */}
+        <div class="flex-auto blockquote-wrapper flex mr-10">
+          <div class="blockquote relative w-11/12 self-center">
+            <h1 className="">
+              Intuitive design happens when current knowledge is the same as the target knowledge.
+            </h1>
+            <h4>—Jared Spool<br /><em>Web Site Usability: A Designer's Guide</em></h4>
+          </div>
+        </div>
+        {/* Their image */}
+        <div class="mt-20 md mt-0">
+          <img src="https://images.unsplash.com/photo-1570136329541-b6a395bc3b4c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="Placeholder image" className="mx-auto w-96 h-96 object-cover"/>
+        </div>
+      </section>
+
+    {/* <section class="hidden md:flex flex-col md:flex-row mt-10 mx-20">
+        <div class="md:w-1/2">
+          <img src="https://images.unsplash.com/photo-1570136329541-b6a395bc3b4c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" alt="Placeholder image" className="mx-auto w-96 h-96" />
+        </div>
+        <div class="flex-auto blockquote-wrapper flex mx-auto">
+          <div class="blockquote relative max-w-[620px] self-center">
+            <h1 className="">
+              Intuitive design happens when current knowledge is the same as the target knowledge.
+            </h1>
+            <h4>—Jared Spool<br /><em>Web Site Usability: A Designer's Guide</em></h4>
+          </div>
+        </div>
+      </section>  */}
+    </div>
+  );
+}
+
+export default Quotes;
