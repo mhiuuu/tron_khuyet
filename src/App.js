3498db006334237c190680d2ecd396a71372a8b5
Mai Trung Hieu (Mathew) <maitrunghieu2287@gmail.com>
2023-08-20 18:51:22 +0700

    Fix the responsive bug on mobile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* Import the styling */
 import './Styling/index.css'
 import './Styling/Hero_text.css'
+import './Styling/Hero.css'
 
 /* Import other components */
 import Hero from './Components/Hero';
@@ -13,11 +14,19 @@ import Header from './Components/Navbar';
 function App() {
   return(
     <div>
+      {/* The navbar */}
       <section>
         <Header />
       </section> 
+
+      {/* The first effect you will see */}
       <section>
         <Hero />
+      </section>
+
+      {/* The gallery area */}
+      <section>
+
       </section>
     </div>
   );
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,9 +3,7 @@ const Hero = () => {
     <div>
       <section className="h-[200vh]">
         <div className="hero_container container_solid top-0 bg-white">
-          {/* <div className="title_wrapper">
-            <h1 className="bg-[100vw_auto] ">The Great Outdoors</h1>
-          </div> */}
+          {/* The white box where the text animation oocur */}
           <div className="title_wrapper">
             <h1 className="cool">
               <span data-text="Tron" className="mr-4">Tron </span>
@@ -13,16 +11,13 @@ const Hero = () => {
             </h1>
           </div>
         </div>
-        <div className="hero_container bg-[url(https://images.unsplash.com/photo-1575058752200-a9d6c0f41945?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ)] bg-[100vw_auto] bg-center bg-fixed top-[100vh]" aria-hidden="true">
+        {/* Where the magic happens */}
+        <div className="hero_container bg-[url(https://images.unsplash.com/photo-1575058752200-a9d6c0f41945?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ)] bg-[100vw_auto] bg-center bg-fixed bg-cover top-[100vh] bg-no-repeat" aria-hidden="true">
           <div className="title_wrapper">
             <h1 className="text-white cool">Tron Khuyet</h1>
           </div>
         </div>
       </section> 
-
-      {/* <div className="h-screen flex flex-col justify-center items-center top-0">
-        
-      </div> */}
   </div>
   );
 };
