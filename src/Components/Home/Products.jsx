85e4c64209ca4bef65e98e48356ca58d065d8cd6
Mai Trung Hieu (Mathew) <maitrunghieu2287@gmail.com>
2023-09-06 22:38:38 +0700

    Almost finish

--- src/Components/Home/Products.jsx
+++ src/Components/Home/Products.jsx
@@ -13,16 +13,12 @@ const Products = () => {
     {src:'https://verentas.sirv.com/UPPD/Copy%20of%20DSC05799.JPG'},
     {src:'https://verentas.sirv.com/UPPD/Copy%20of%20DSC05873.JPG'},
     {src:'https://verentas.sirv.com/UPPD/Copy%20of%20DSC05880.JPG'},
-    {src:'https://verentas.sirv.com/UPPD/Copy%20of%20DSC05890.JPG'},
-    {src:'https://verentas.sirv.com/UPPD/DSC07481.JPG'},
     {src:'https://verentas.sirv.com/UPPD/DSC07502.JPG'},
     {src:'https://verentas.sirv.com/UPPD/DSC07503.JPG'},
     {src:'https://verentas.sirv.com/UPPD/DSC07504.JPG'},
     {src:'https://verentas.sirv.com/UPPD/DSC07505.JPG'},
     {src:'https://verentas.sirv.com/UPPD/DSC07506.JPG'},
-    {src:'https://verentas.sirv.com/UPPD/DSC07636.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_1281.JPG'},
-    {src:'https://verentas.sirv.com/UPPD/IMG_1283.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_1373.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_1381.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_1459.JPG'},
@@ -33,11 +29,8 @@ const Products = () => {
     {src:'https://verentas.sirv.com/UPPD/IMG_1473.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_1475.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_1477.JPG'},
-    {src:'https://verentas.sirv.com/UPPD/IMG_2912.JPG'},
-    {src:'https://verentas.sirv.com/UPPD/IMG_2913.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_2916.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_2925.JPG'},
-    {src:'https://verentas.sirv.com/UPPD/IMG_2926.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_3015.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_3025.JPG'},
     {src:'https://verentas.sirv.com/UPPD/IMG_3036.JPG'},
@@ -49,8 +42,8 @@ const Products = () => {
         <div className="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
           {/* The static part */}
           <div className="flex flex-col w-full sticky md:top-36 lg:w-1/2 mt-2 md:mt-12 px-16 lg:px-36">
-            <p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">Working Process of Fest</p>
-            <p className="text-sm md:text-base text-black mb-12">
+            <p className="text-3xl md:text-4xl mb-2">Các sản phẩm</p>
+            <p className="text-sm md:text-base text-black my-12">
               Bỷe bủh bủuh lmao hihi haha fỏk u kikiki 
             </p>
           </div>
