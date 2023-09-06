829d0c01727a674ceabd65dce9328b64460db9d6
Mai Trung Hieu (Mathew) <maitrunghieu2287@gmail.com>
2023-09-06 18:26:21 +0700

    Working to final product

--- src/Components/Home/Gallery.jsx
+++ src/Components/Home/Gallery.jsx
@@ -11,73 +11,18 @@ const Ga = ({ img }) => {
 const Imgs = () => {
   const Assets = [
     { src: 'https://verentas.sirv.com/UPPP/DSC07830.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/DSC07474.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/DSC07814.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/DSC07833.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1290.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1294.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1299.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1305.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1307.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1312.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1316.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1321.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1334.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1336.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1341.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1349.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1350.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1351.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1354.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1355.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1358.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1361.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1362.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1363.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1364.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1365.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1366.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1368.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1371.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1384.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1395.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1398.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1404.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1405.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1407.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1413.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1421.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1425.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1426.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1428.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1430.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1431.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1432.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1435.JPG' },
-    /* { src: 'https://verentas.sirv.com/UPPP/IMG_1438.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1441.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1442.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1445.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1446.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1447.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1452.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1456.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_1478.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_2901.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_2903.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_2905.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_2906.JPG' },
-    { src: 'https://verentas.sirv.com/UPPP/IMG_2910.JPG' }, */
-
-
   ];
 
   return (
-    <div class="columns-2 lg:columns-4 gap-x-4 mx-2.5 my-8 container mx-auto">
-      {Assets.map((item) => (
-        <Ga img={item.src} />
-      ))}
+    <div> 
+      <div class="h-1 w-full bg-black mx-2.5"></div>
+      <div class="columns-2 lg:columns-4 gap-x-4 mx-2.5 my-8 container mx-auto">
+        {Assets.map((item) => (
+          <Ga img={item.src} />
+        ))}
     </div>
+    </div>
+    
   );
 };
 
--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -2,12 +2,12 @@ const Hero = () => {
   return (
     <div>
       <section className="h-[190vh]">
-        <div className="hero_container container_solid top-0 bg-[url(https://verentas.sirv.com/backdrop.png)] bg-no-repeat bg-cover backdrop-filter backdrop-blur">
+        <div className="hero_container container_solid top-0 bg-white bg-no-repeat bg-cover backdrop-filter backdrop-blur">
           {/* The white box where the text animation oocur */}
           <div className="title_wrapper">
             <h1 className="cool">
-              <span data-text="Tron" className="mr-4">Tron </span>
-              <span data-text="Khuyet " className="mr-4">Khuyet </span>
+              <span data-text="Trọn" className="mr-4">Trọn </span>
+              <span data-text="Khuyết " className="mr-4">Khuyết </span>
             </h1>
           </div>
         </div>
